b20dcc1d68c042db8d3cd3022fc387e0fce5d155
Rishit Chaudhary <rishit.c@live.com>
2021-06-23 22:23:53 +0530

    Make all annotations behave like forward annotations

--- src/megha_sim/simulator_utils/globals.py
+++ src/megha_sim/simulator_utils/globals.py
@@ -6,6 +6,7 @@ as well as the start times of each of the jobs given as input to
 the simulator from the trace file.
 """
 
+from __future__ import annotations
 from typing import Dict, List, TYPE_CHECKING
 
 if TYPE_CHECKING:
