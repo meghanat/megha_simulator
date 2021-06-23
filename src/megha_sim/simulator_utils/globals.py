e596102386104a411c5638f59487c2ceec3b8c88
Rishit Chaudhary <rishit.c@live.com>
2021-06-23 21:59:13 +0530

    Added documentation and typing

--- src/megha_sim/simulator_utils/globals.py
+++ src/megha_sim/simulator_utils/globals.py
@@ -1,2 +1,15 @@
-job_start_tstamps = {}
-jobs_completed = []
+"""
+This file contains the global variables used in the simulator.
+
+The global variables are used to store the list of jobs completed
+as well as the start times of each of the jobs given as input to
+the simulator from the trace file.
+"""
+
+from typing import Dict, List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from job import Job
+
+job_start_tstamps: Dict[float, float] = {}
+jobs_completed: List[Job] = []
