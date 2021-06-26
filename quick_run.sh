55925601f2c4b0956b4a6aa64e17c7b13814c117
Rishit Chaudhary <rishit.c@live.com>
2021-06-26 23:04:46 +0530

    Added both commands to run one after the other.

--- quick_run.sh
+++ quick_run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python3 ./src/runner.py ./traces/input/YH.tr ./simulator_config/config.json 3 2 3 1 1 1 
 python3 ./src/runner.py ./traces/input/YH.tr ./simulator_config/config.json 3 2 3 1 1 1 > traces/output/YH_OP.tr
