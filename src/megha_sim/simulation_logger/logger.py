c9d4b8de82093da48bc3f294baeb68227491f357
Rishit Chaudhary <rishit.c@live.com>
2021-06-28 14:16:58 +0530

    Created Logger class

---
+++ src/megha_sim/simulation_logger/logger.py
@@ -0,0 +1,44 @@
+"""
+Create the logging object to use in the different components of the simulator.
+
+The logging object is used to log different events in the scheduler in a
+detailed and uniform manner for analysis.
+"""
+import logging
+from pathlib import Path
+from datetime import datetime
+
+
+class SimulatorLogger:
+    """This class is to define and create instances of the logging class."""
+
+    LOG_FILE_PATH = Path("logs")
+    LOG_FORMAT = ("%(asctime)s : %(module)s : %(filename)s : %(funcName)s : "
+                  "%(lineno)d : %(levelname)s : %(message)s")
+
+    def __init__(self, module_name: str):
+        """
+        Initialize the object with the name of the caller code's module name.
+
+        Args:
+            module_name (str): Name of the caller code's module
+        """
+        self.module_name = module_name
+        self.LOG_FILE_NAME = self.LOG_FILE_PATH / (datetime.now()
+                                                   .strftime("record-%Y-%m-%d-"
+                                                             "%H-%M-%S.log"))
+        with open(self.LOG_FILE_NAME, "a"):
+            ...
+        logging.basicConfig(filename=self.LOG_FILE_NAME,
+                            format=self.LOG_FORMAT,
+                            # encoding='utf-8',  # Only for Python >= 3.9
+                            level=logging.INFO)
+
+    def get_logger(self) -> logging.Logger:
+        """
+        Return a configured instance of the logging class to the caller.
+
+        Returns:
+            logging.Logger: Object used for logging runtime information.
+        """
+        return logging.getLogger(self.module_name)
