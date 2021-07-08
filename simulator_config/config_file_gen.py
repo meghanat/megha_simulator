9e0f5058889d8e561efa683fa0832888b1bdccae
Rishit Chaudhary <rishit.c@live.com>
2021-07-08 15:39:45 +0530

    Added linting to config file generator.

--- simulator_config/config_file_gen.py
+++ simulator_config/config_file_gen.py
@@ -9,18 +9,19 @@ cluster_config["LMs"] = {}
 
 for i in range(1, num_lms+1):
     lm_id = str(i)
-    cluster_config["LMs"][lm_id] = {"LM_id":str(i), "partitions":{}}
+    cluster_config["LMs"][lm_id] = {"LM_id": str(i), "partitions": {}}
     part_dict = {}
     for j in range(1, num_partitions+1):
-        part_dict[str(j)] = {"partition_id":str(j), "nodes":{}}
-        
+        part_dict[str(j)] = {"partition_id": str(j), "nodes": {}}
+
         nodes_dict = {}
         for k in range(1, partition_size+1):
-            nodes_dict[str(k)] = {"CPU":1, "RAM":1, "DISK":1, "constraints":[]}
-            
+            nodes_dict[str(k)] = {"CPU": 1, "RAM": 1,
+                                  "DISK": 1, "constraints": []}
+
         part_dict[str(j)]["nodes"] = nodes_dict
-    
+
     cluster_config["LMs"][lm_id]["partitions"] = part_dict
 
-string = json.dumps(cluster_config, indent = 4)
+string = json.dumps(cluster_config, indent=4)
 print(string)
