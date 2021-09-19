06020289b2d06596afc6c21e4e85be4ea96bef42
Rishit Chaudhary <rishit.c@live.com>
2021-09-19 22:43:35 +0530

    Fixed Docker build issue.

--- enter_dev_env.sh
+++ enter_dev_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t megha_dev_env_python -f Dockerfile_python
+docker build -t megha_dev_env_python -f Dockerfile_python .
 docker run -it --rm --name megha_dev_env_python -v "${PWD}":/Megha_Dev megha_dev_env_python:latest bash
