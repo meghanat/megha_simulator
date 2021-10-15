9ac1b650e22f265039e8dc248f98d7ec869b8766
Rishit Chaudhary <rishit.c@live.com>
2021-10-16 00:00:14 +0530

    Added quick run scripts.

--- create_dev_env_pypy.sh
+++ create_dev_env_pypy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 docker build -t megha_dev_env .
-docker run -it -d --rm --name megha_dev_env -v "${PWD}":/Megha_Dev megha_dev_env:latest bash
+
+# 1st container
+docker run -it -d --rm --name megha_dev_env_1 -v "${PWD}":/Megha_Dev megha_dev_env:latest bash
+
+# 2nd container
+docker run -it -d --rm --name megha_dev_env_2 -v "${PWD}":/Megha_Dev megha_dev_env:latest bash
+
+# 3rd container
+docker run -it -d --rm --name megha_dev_env_3 -v "${PWD}":/Megha_Dev megha_dev_env:latest bash
+
+# 4th container
+docker run -it -d --rm --name megha_dev_env_4 -v "${PWD}":/Megha_Dev megha_dev_env:latest bash
