d5a2f4df0421b807840ce0d97ed32fded07daefc
Rishit Chaudhary <rishit.c@live.com>
2021-11-03 14:37:35 +0530

    Updated PyPy quick run script.

--- quick_run_pypy.sh
+++ quick_run_pypy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pypy ./src/runner.py ./traces/input/YH_small.tr ./simulator_config/config_original.json 3 2 3 1 1 1 
-pypy ./src/runner.py ./traces/input/YH_small.tr ./simulator_config/config_original.json 3 2 3 1 1 1 > traces/output/YH_OP.tr
+
+pypy ./src/runner.py ./traces/input/YH_small.tr ./simulator_config/config_original.json 3 2 3 1 1 1 > logs/YH_OP.tr
+
+cat logs/*.log
