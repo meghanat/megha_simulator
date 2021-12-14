74c2db00b1fea2c9cf6ca8d6cc369018c5fe4140
Rishit Chaudhary <rishit.c@live.com>
2021-12-14 19:13:27 +0530

    Updated command list in quick_run.sh

--- quick_run.sh
+++ quick_run.sh
@@ -5,3 +5,4 @@ python3 ./src/runner.py ./traces/input/YH_small.tr ./simulator_config/config_ori
 
 # Run the below command:
 python3 ./src/runner.py ./traces/input/YH_small.tr ./simulator_config/config_1000.json 10 10 10 1 1 1
+python3 ./src/runner.py ./traces/input/GOOG.tr ./simulator_config/config_10_000.json 10 10 100 1 1 1
