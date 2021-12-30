b9ad7f1c90b1b157f1fd774055d6b76b14e7b204
Rishit Chaudhary <rishit.c@live.com>
2021-12-30 21:30:30 +0530

    Exporting debug options for the runner code.

--- src/megha_sim/__init__.py
+++ src/megha_sim/__init__.py
@@ -1,3 +1,4 @@
 from simulation import Simulation
 import simulator_utils.globals as simulator_globals
+from simulator_utils import debug_print, DEBUG_MODE
 from simulation_logger import SimulatorLogger
